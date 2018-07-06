f18ef33e84c57b5bcf14e98933cafb55e5384354
panjie <3792535@qq.com>
2018-07-06 08:31:15 +0800

    重构代码

--- multiQuery/src/main/java/com/mengyunzhi/springbootsamplecode/multiquery/service/YunzhiServiceImpl.java
+++ multiQuery/src/main/java/com/mengyunzhi/springbootsamplecode/multiquery/service/YunzhiServiceImpl.java
@@ -19,10 +19,12 @@ import javax.persistence.criteria.*;
 import java.lang.reflect.Field;
 import java.sql.Date;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.List;
 
 /**
  * 多条件综合查询
+ *
  * @author panjie
  */
 @Service
@@ -75,8 +77,83 @@ public class YunzhiServiceImpl implements YunzhiService {
                                 continue;
                             }
 
-                            if (field.getAnnotation(ManyToOne.class) != null) {
-                                logger.debug("有manyToOne的注解");
+                            // 初始化两个界限的变量
+                            Boolean isBegin = false;
+                            Boolean isEnd = false;
+
+                            // 查找开始与结束的注解
+                            BeginQueryParam beginQueryParam = field.getAnnotation(BeginQueryParam.class);
+                            if (beginQueryParam != null) {
+                                logger.debug("存在@BeginQueryParam注解");
+                                isBegin = true;
+                                name = beginQueryParam.name();
+                            } else if (field.getAnnotation(EndQueryParam.class) != null) {
+                                logger.debug("存在@EndQueryParam注解");
+                                isEnd = true;
+                                name = field.getAnnotation(EndQueryParam.class).name();
+                            }
+
+                            // 按字段类型进行查询
+                            if (value instanceof String) {
+                                logger.debug("字符串则进行模糊查询");
+                                String stringValue = ((String) value);
+                                if (!stringValue.isEmpty()) {
+                                    this.andPredicate(criteriaBuilder.like(root.get(name).as(String.class), "%" + stringValue + "%"));
+                                }
+                            } else if (value instanceof Number) {
+                                logger.debug("如果为number，则进行精确或范围查询");
+                                if (value instanceof Short) {
+                                    Short shortValue = (Short) value;
+                                    if (isBegin) {
+                                        this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Short.class), shortValue));
+                                    } else if (isEnd) {
+                                        this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Short.class), shortValue));
+                                    } else {
+                                        this.andPredicate(criteriaBuilder.equal(root.get(name).as(Short.class), shortValue));
+                                    }
+                                } else if (value instanceof Integer) {
+                                    Integer integerValue = (Integer) value;
+                                    if (isBegin) {
+                                        this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Integer.class), integerValue));
+                                    } else if (isEnd) {
+                                        this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Integer.class), integerValue));
+                                    } else {
+                                        this.andPredicate(criteriaBuilder.equal(root.get(name).as(Integer.class), integerValue));
+                                    }
+                                } else if (value instanceof Long) {
+                                    Long longValue = (Long) value;
+                                    if (isBegin) {
+                                        this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Long.class), longValue));
+                                    } else if (isEnd) {
+                                        this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Long.class), longValue));
+                                    } else {
+                                        this.andPredicate(criteriaBuilder.equal(root.get(name).as(Long.class), longValue));
+                                    }
+                                } else {
+                                    logger.error("综合查询Number类型，暂时只支持到Short,Integer,Long");
+                                }
+                            } else if (value instanceof Calendar) {
+                                logger.debug("Calendar类型");
+                                Calendar calendarValue = (Calendar) value;
+                                if (isBegin) {
+                                    this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Calendar.class), calendarValue));
+                                } else if (isEnd) {
+                                    this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Calendar.class), calendarValue));
+                                } else {
+                                    this.andPredicate(criteriaBuilder.equal(root.get(name).as(Calendar.class), calendarValue));
+                                }
+                            } else if (value instanceof Date) {
+                                logger.debug("Sql.Date类型");
+                                Date dateValue = (Date) value;
+                                if (isBegin) {
+                                    this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Date.class), dateValue));
+                                } else if (isEnd) {
+                                    this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Date.class), dateValue));
+                                } else {
+                                    this.andPredicate(criteriaBuilder.equal(root.get(name).as(Date.class), dateValue));
+                                }
+                            } else if (value instanceof YunZhiAbstractEntity) {
+                                logger.debug("是实体类");
                                 YunZhiAbstractEntity yunZhiAbstractEntity = (YunZhiAbstractEntity) value;
                                 if (yunZhiAbstractEntity.getId() != null) {
                                     logger.debug("对应的ManyToOne，加入了id, 则按ID查询");
@@ -86,92 +163,14 @@ public class YunzhiServiceImpl implements YunzhiService {
                                     this.generatePredicate(value, root.join(name));
                                 }
 
-                            } else if (field.getAnnotation(OneToMany.class) != null) {
-                                logger.error("暂不支持OneToMany注解");
-
-                            } else if (field.getAnnotation(ManyToMany.class) != null) {
-                                logger.error("暂不支持ManyToMany注解");
-
+                            } else if (value instanceof Collection) {
+                                Collection<?> collectionValue = (Collection<?>)value;
+                                if (collectionValue.size() > 0) {
+                                    logger.warn("暂不支持一对多，多对多查询");
+                                    // todo: 一对多，多对多查询
+                                }
                             } else {
-                                logger.debug("没有@ManyToOne, OneToMany, ManyToMany注解，说明为普通字段, 按value类型，进行综合查询");
-                                // 初始化两个界限的变量
-                                Boolean isBegin = false;
-                                Boolean isEnd = false;
-
-                                // 查找开始与结束的注解
-                                BeginQueryParam beginQueryParam = field.getAnnotation(BeginQueryParam.class);
-                                if (beginQueryParam != null) {
-                                    logger.debug("存在@BeginQueryParam注解");
-                                    isBegin = true;
-                                    name = beginQueryParam.name();
-                                } else if (field.getAnnotation(EndQueryParam.class) != null) {
-                                    logger.debug("存在@EndQueryParam注解");
-                                    isEnd = true;
-                                    name = field.getAnnotation(EndQueryParam.class).name();
-                                }
-
-                                // 按字段类型进行查询
-                                if (value instanceof String) {
-                                    logger.debug("字符串则进行模糊查询");
-                                    String stringValue = ((String) value);
-                                    if (!stringValue.isEmpty()) {
-                                        this.andPredicate(criteriaBuilder.like(root.get(name).as(String.class), "%" + stringValue + "%"));
-                                    }
-                                } else if (value instanceof Number) {
-                                    logger.debug("如果为number，则进行精确或范围查询");
-                                    if (value instanceof Short) {
-                                        Short shortValue = (Short) value;
-                                        if (isBegin) {
-                                            this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Short.class), shortValue));
-                                        } else if (isEnd) {
-                                            this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Short.class), shortValue));
-                                        } else {
-                                            this.andPredicate(criteriaBuilder.equal(root.get(name).as(Short.class), shortValue));
-                                        }
-                                    } else if (value instanceof Integer) {
-                                        Integer integerValue = (Integer) value;
-                                        if (isBegin) {
-                                            this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Integer.class), integerValue));
-                                        } else if (isEnd) {
-                                            this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Integer.class), integerValue));
-                                        } else {
-                                            this.andPredicate(criteriaBuilder.equal(root.get(name).as(Integer.class), integerValue));
-                                        }
-                                    } else if (value instanceof Long) {
-                                        Long longValue = (Long) value;
-                                        if (isBegin) {
-                                            this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Long.class), longValue));
-                                        } else if (isEnd) {
-                                            this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Long.class), longValue));
-                                        } else {
-                                            this.andPredicate(criteriaBuilder.equal(root.get(name).as(Long.class), longValue));
-                                        }
-                                    } else {
-                                        logger.error("综合查询Number类型，暂时只支持到Short,Integer,Long");
-                                    }
-                                } else if (value instanceof Calendar) {
-                                    logger.debug("Calendar类型");
-                                    Calendar calendarValue = (Calendar) value;
-                                    if (isBegin) {
-                                        this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Calendar.class), calendarValue));
-                                    } else if (isEnd) {
-                                        this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Calendar.class), calendarValue));
-                                    } else {
-                                        this.andPredicate(criteriaBuilder.equal(root.get(name).as(Calendar.class), calendarValue));
-                                    }
-                                } else if (value instanceof Date) {
-                                    logger.debug("Sql.Date类型");
-                                    Date dateValue = (Date) value;
-                                    if (isBegin) {
-                                        this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Date.class), dateValue));
-                                    } else if (isEnd) {
-                                        this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Date.class), dateValue));
-                                    } else {
-                                        this.andPredicate(criteriaBuilder.equal(root.get(name).as(Date.class), dateValue));
-                                    }
-                                } else {
-                                    logger.error("暂不支持传入的数据类型");
-                                }
+                                logger.error("综合查询，暂不支持传入的数据类型", name, field);
                             }
                         }
                     }
