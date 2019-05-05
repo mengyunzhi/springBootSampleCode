1f95890935eb43987f7df25653876d42d5c6b443
panjie <3792535@qq.com>
2019-05-05 12:11:54 +0800

    增加注释

--- jsonview/src/test/java/com/mengyunzhi/springBootSampleCode/jsonview/KlassControllerTest.java
+++ jsonview/src/test/java/com/mengyunzhi/springBootSampleCode/jsonview/KlassControllerTest.java
@@ -3,15 +3,20 @@ package com.mengyunzhi.springBootSampleCode.jsonview;
 import com.alibaba.fastjson.JSON;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
 @AutoConfigureMockMvc
-public class KlassControllerTest extends  JsonviewApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KlassControllerTest {
     @Autowired
     private KlassRepository klassRepository;
     @Autowired
@@ -22,21 +27,22 @@ public class KlassControllerTest extends  JsonviewApplicationTests {
 
     @Test
     public void getById() throws Exception {
+        // 数据准备
         Klass klass = new Klass("测试班级");
         klassRepository.save(klass);
-
         Student student = new Student("测试学生");
         student.setKlass(klass);
         studentRepository.save(student);
-
         klass.getStudents().add(student);
         klassRepository.save(klass);
 
+        // 模拟请求，将结果转化为字符化
         String result = this.mockMvc.perform(
                 MockMvcRequestBuilders.get("/klass/" + klass.getId().toString())
                         .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andReturn().getResponse().getContentAsString();
 
+        // 将字符串转换为实体，并断言
         Klass resultKlass = JSON.parseObject(result, Klass.class);
         Assertions.assertThat(resultKlass.getName()).isEqualTo("测试班级");
         Assertions.assertThat(resultKlass.getStudents().size()).isEqualTo(1);
--- jsonview/src/test/java/com/mengyunzhi/springBootSampleCode/jsonview/StudentControllerTest.java
+++ jsonview/src/test/java/com/mengyunzhi/springBootSampleCode/jsonview/StudentControllerTest.java
@@ -3,15 +3,20 @@ package com.mengyunzhi.springBootSampleCode.jsonview;
 import com.alibaba.fastjson.JSON;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
 @AutoConfigureMockMvc
-public class StudentControllerTest extends JsonviewApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentControllerTest {
     @Autowired
     private KlassRepository klassRepository;
     @Autowired
@@ -22,19 +27,20 @@ public class StudentControllerTest extends JsonviewApplicationTests {
 
     @Test
     public void getById() throws Exception {
+        // 数据准备
         Klass klass = new Klass("测试班级");
         klassRepository.save(klass);
-
         Student student = new Student("测试学生");
         student.setKlass(klass);
         studentRepository.save(student);
 
-
+        // 模拟请求，将结果转化为字符化
         String result = this.mockMvc.perform(
                 MockMvcRequestBuilders.get("/student/" + student.getId().toString())
                         .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andReturn().getResponse().getContentAsString();
 
+        // 将字符串转换为实体，并断言
         Student resultStudent = JSON.parseObject(result, Student.class);
         Assertions.assertThat(resultStudent.getName()).isEqualTo("测试学生");
         Assertions.assertThat(resultStudent.getKlass().getName()).isEqualTo("测试班级");
