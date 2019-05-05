9b78e02524f397144cc64d0feda4ba0f10c40dd0
panjie <3792535@qq.com>
2019-05-05 08:20:34 +0800

    init

---
+++ jsonview/src/main/java/com/mengyunzhi/springBootSampleCode/jsonview/JsonviewApplication.java
@@ -0,0 +1,13 @@
+package com.mengyunzhi.springBootSampleCode.jsonview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonviewApplication.class, args);
+	}
+
+}
---
+++ jsonview/src/test/java/com/mengyunzhi/springBootSampleCode/jsonview/JsonviewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mengyunzhi.springBootSampleCode.jsonview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JsonviewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
